7e6cfccef0349315f61a0b7f83ff4ca7e3eee031
Alamin-Coding <alamin.codelover@gmail.com>
2022-12-01 11:02:34 +0600

    responsive file add

--- js/script.js
+++ js/script.js
@@ -9,5 +9,12 @@ $(function(){
         arrows:false,
         focusOnSelect:true
     })
+
+    // navbar-toggler
+
+    // let navbar_toggler = document.querySelector(".navbar-toggler");
+    // navbar_toggler.addEventListener("click", function() {
+    //     navbar_toggler.classList.toggle("color")
+    // })
 })
 
