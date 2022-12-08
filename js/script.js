6318c5e9985b27994945382b49ccb6ff8e65edf8
Alamin-Coding <alamin.codelover@gmail.com>
2022-12-08 11:05:39 +0600

    Small device responsive done

--- js/script.js
+++ js/script.js
@@ -7,7 +7,33 @@ $(function(){
         centerMode:true,
         centerPadding: "150px",
         arrows:false,
-        focusOnSelect:true
+        focusOnSelect:true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                centerMode:false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     })
 
     // navbar-toggler
